4d75900c81f22704de510bcfdc805cb1561d1e93
Nathan Agpalo <nathana7221@gmail.com>
2022-09-16 13:38:08 -0400

    added visualization exercise code for lab 2

--- Lab1/lab1_part2.r
+++ Lab1/lab1_part2.r
@@ -1,3 +1,6 @@
+
+EPI_DATA = read.csv("/Users/naytun/Downloads/EPI_DATA.csv")
+
 help("qqnorm")
 qqnorm(EPI_DATA$EPI)
 qqline(EPI_DATA$EPI)
@@ -11,10 +14,21 @@ qqplot(qt(ppoints(250),df=5),x, xlab = "Q-Q plot")
 qqline(x)
 install.packages("readxl")
 library("readxl")
-multivariate = read.csv("/Users/naytun/Downloads/multivariate.csv")
+help(c)
+multivariate <- read_xls("/Users/naytun/Downloads/multivariate.xls")
 multivariate
+multivariate = multivariate[c(1:7), c(1:6)]
+View(multivariate)
 attach(multivariate)
 ?attach
 ?lm
 mm = lm(Homeowners~Immigrants)
 plot(Homeowners~Immigrants)
+summary(mm)
+help(abline)
+abline(mm)
+abline(mm, col=2, lwd=3)
+abline(mm)
+abline(mm, col=3, lwd=3)
+attributes(mm)
+mm$coefficients
