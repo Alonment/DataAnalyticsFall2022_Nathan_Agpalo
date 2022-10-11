570941d09586ff9f6e9017e02f83984bcb1d1ada
Nathan Agpalo <nathana7221@gmail.com>
2022-10-11 13:25:16 -0400

    some updates to lab 3

--- Lab3/lab3.r
+++ Lab3/lab3.r
@@ -37,3 +37,29 @@ par(mfrow = c(1,3))
 image(t(dataMatOrdered)[, nrow(dataMatOrdered):1])
 plot(rowMeans(dataMatOrdered), 40:1, xlab="The Row Mean", ylab="Row", pch=19)
 plot(colMeans(dataMatOrdered), xlab="Column", ylab="The Column Mean", pch=19)
+
+install.packages("titanic")
+
+library(titanic)
+attach(titanic_train)
+
+colnames(titanic_train)
+
+library(rpart)
+?rpart
+
+head(titanic_train)
+fit_rpart = rpart(Survived ~ ., data=titanic_train, method="class")
+printcp(fit_rpart)
+plotcp(fit_rpart)
+text(fit_rpart)
+
+install.packages("party")
+library(party)
+help(ctree)
+help(sapply)
+
+sapply(titanic_train, "class") # Determine which predictors aren't charcaters and use them as input
+fit_ctree = ctree(Survived ~ PassengerId + Pclass + Age + SibSp + Parch + Fare, data=titanic_train, )
+plot(fit_ctree)
+
